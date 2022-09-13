28a0c02ac1649c86eb5920370274b008318d9d40
Kai Franke <cosinushh@gmail.com>
2022-09-13 15:44:00 +0200

    create array of all cards

---
+++ components/card-list/card-content.js
@@ -0,0 +1,34 @@
+const cards = [
+  {
+    question: "first question",
+    answer: "first answer",
+    tags: "first tag",
+    bookmarked: true,
+  },
+  {
+    question: "second question",
+    answer: "second answer",
+    tags: "second tag",
+    bookmarked: false,
+  },
+  {
+    question: "third question",
+    answer: "third answer",
+    tags: "third tag",
+    bookmarked: true,
+  },
+  {
+    question: "fourth question",
+    answer: "fourth answer",
+    tags: "fourth tag",
+    bookmarked: false,
+  },
+  {
+    question: "fifth question",
+    answer: "fifth answer",
+    tags: "fifth tag",
+    bookmarked: true,
+  },
+];
+
+export default cards;
