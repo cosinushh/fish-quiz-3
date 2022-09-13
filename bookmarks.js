d69d79f95cda6ebba0e499902f95540a8898a79d
Kai Franke <cosinushh@gmail.com>
2022-09-13 16:37:55 +0200

    generate cards from array in index and bookmarks.js

--- bookmarks.js
+++ bookmarks.js
@@ -1,12 +1,24 @@
 import createCard from "./components/card/card.js";
+import cards from "./components/card-list/card-content.js";
 
 const cardContainer = document.querySelector('[data-js="card-list"]');
 
-const card5 = createCard("question 5", "answer 5", "tag 5");
-cardContainer.append(card5);
+// cards.forEach((card) => {
+//   if (card.bookmarked) {
+//     const newCard = createCard(card.question, card.answer, card.tags);
+//     newCard
+//       .querySelector('[data-js="bookmark-button"]')
+//       .classList.add("bookmark--active");
+//     cardContainer.append(newCard);
+//   }
+// });
 
-const card6 = createCard("question 6", "answer 6", "tag 6");
-cardContainer.append(card6);
-
-const card7 = createCard("question 7", "answer 7", "tag 7");
-cardContainer.append(card7);
+cards
+  .filter((card) => (card.bookmarked ? true : false))
+  .forEach((item) => {
+    const newCard = createCard(item.question, item.answer, item.tags);
+    newCard
+      .querySelector('[data-js="bookmark-button"]')
+      .classList.add("bookmark--active");
+    cardContainer.append(newCard);
+  });
--- index.js
+++ index.js
@@ -1,32 +1,9 @@
 import createCard from "./components/card/card.js";
+import cards from "./components/card-list/card-content.js";
 
 const cardContainer = document.querySelector('[data-js="card-list"]');
-// const answerButton = document.querySelector('[data-js="answer-button"]');
-// const answer = document.querySelector('[data-js="card-answer"]');
-// const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
 
-// answerButton.addEventListener("click", () => {
-//   if (answer.classList.contains("card__answer--active")) {
-//     answer.classList.remove("card__answer--active");
-//     answerButton.textContent = "Show answer";
-//   } else {
-//     answer.classList.add("card__answer--active");
-//     answerButton.textContent = "Hide answer";
-//   }
-// });
-
-// bookmarkButton.addEventListener("click", () => {
-//   bookmarkButton.classList.toggle("bookmark--active");
-// });
-
-const card1 = createCard("question 1", "answer 1", "tag 1");
-cardContainer.append(card1);
-
-const card2 = createCard("question 2", "answer 2", "tag 2");
-cardContainer.append(card2);
-
-const card3 = createCard("question 3", "answer 3", "tag 3");
-cardContainer.append(card3);
-
-const card4 = createCard("question 4", "answer 4", "tag 4");
-cardContainer.append(card4);
+cards.forEach((card) => {
+  const newCard = createCard(card.question, card.answer, card.tags);
+  cardContainer.append(newCard);
+});
